4fc5c3d2b13ce8fe70d18dd43130e32264b2e4b2
Mehdi Ali Qoli <162902639+MehdiAliQoli@users.noreply.github.com>
2024-11-10 22:00:08 +0500

    Add files via upload

---
+++ Dectector.py
@@ -0,0 +1,25 @@
+import tenserflow as tf
+(x_train, y_train),(x_test,y_test) = tf.keras.datasets.mnist.load_data()
+x_train , x_test = x_train/255.0, x_test/255.0
+input_layer = tf.keras.layers.Flatten(input_shape=(28, 28))
+hidden1 = tf.keras.layers.Dense(16,activation='sigmoid')
+hidden2 = tf.keras.layers.Dense(16,activation = 'sigmoid')
+out_put_layer = tf.keras.layers.Dense(10,activation='softmax')
+
+
+models = tf.keras.Sequential(
+    [input_layer,
+     hidden1,
+     hidden2,
+     out_put_layer
+
+     ]
+)
+models.compile(optimizer='sgd',
+loss = 'sparse_categorical_crossentropy',
+metrics = ['accuracy']
+)
+models.fit(x_train, y_train, epochs=8)
+test_accuracy = models.evaluate(x_test, y_test)
+
+
